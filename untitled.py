d2a05c0d4ccbd942691aed25ffbf458d749f71fa
ashley-o <53440511+ashley-o@users.noreply.github.com>
2019-07-31 10:10:50 -0400

    Add files via upload

---
+++ untitled.py
@@ -0,0 +1,16 @@
+"""
+cadbury = (15)
+cadburybox = cadbury + 5 
+print('You have' ,cadburybox, 'choclates')
+
+cadburymilk= ('milk chocolate')
+cadburydark= ('dark chocolate')
+cadburywhite= ('white chocolate') 
+print('There is' ,cadburymilk,',',cadburydark,'and',cadburywhite,'.')
+
+name=input('Please enter your name')
+print('Your name is',name)
+"""
+age= int(input())
+
+print('You are' ,age, 'years old')
---
+++ untitled1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 13:24:56 2019
+
+@author: STEM
+"""
+print("Hellow World")
+sample = input("Hi")
+
+import math
+dir(math)
+math.pow(6,0.5)
+
+number=int(input('Please enter a value for your variable:'))
+answer = math.pow(number,1/3)
+print('The cubic root of' ,number, 'is' ,answer, )
