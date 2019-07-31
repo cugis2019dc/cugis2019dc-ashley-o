d8a0c4cd381d25543cfa875740a47e7423006479
ashley-o <53440511+ashley-o@users.noreply.github.com>
2019-07-31 14:57:19 -0400

    Add files via upload

--- listsanddicts.py
+++ listsanddicts.py
@@ -15,11 +15,25 @@ cadburylist= [['milk',5,],['dark',8],['white',3]]
 import pandas
 dir(pandas)
 cadburydf=pandas.DataFrame(cadburylist,columns=('Type Of Chocolate','Quantity'))
-print(cadburydf)
-
+print(cadburydf['Type Of Chocolate'])
+print(cadburydf['Quantity'])
 name=input('Please enter your name')
-print('Your name is',name)
+print('Your name is',name) 
 
+import plotly
+dir(plotly)
+
+from plotly.offline import plot 
+import plotly.graph_objs as go
+cadburyBar = go.Bar(x=cadburydf['Type Of Chocolate'],y=cadburydf['Quantity'])
+plot([cadburyBar])
+
+titles = go.Layout(title = 'Number Of Chocolates By Type')
+
+fig=go.Figure(data=[cadburyBar], layout=titles)
+plot(fig)
+
+'''
 age= int(input())
 
 print('You are' ,age, 'years old')
@@ -35,5 +49,21 @@ studentlist= [['Steve',32,'M',],['Lia',28,'F'],['Vin',45,'M'], ['Katie',38,'F']]
 import pandas
 dir(pandas)
 
-studentdf = pandas.DataFrame(studentlist,columns=('Name','Age','Gender'),rows=('1','2','3'))
+studentdf = pandas.DataFrame(studentlist,columns=('Name','Age','Gender'),index=['1','2','3','4'])
 print(studentdf)
+
+print(studentdf['Name'])
+print(studentdf['Age'])
+print(studentdf['Gender'])
+
+import plotly
+dir(plotly)
+from plotly.offline import plot
+import plotly.graph_objs as go
+studentBar = go.Bar(x=studentdf['Name'],y=studentdf['Age'])
+plot([studentBar]) 
+'''
+
+
+
+
